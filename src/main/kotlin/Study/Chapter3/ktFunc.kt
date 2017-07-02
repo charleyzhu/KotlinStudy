b37f7346b3ff748f47c8b3c2967658f00f1e426e
Charley <2555085@qq.com>
2017-07-03 01:10:05 +0800

    初略的学习下kotlin的常量和变量

---
+++ src/main/kotlin/Study/Chapter3/ktFunc.kt
@@ -0,0 +1,2 @@
+package Study.Chapter3
+
---
+++ src/main/kotlin/Study/Chapter3/ktVar&Val.kt
@@ -0,0 +1,10 @@
+package Study.Chapter3
+
+var aVar:String = "var_HelloWorld" //变量
+val aVal:String = "val_HelloWorld" //常量
+const val aConstVal:String = "ConstVal" //编译器常量 //提高程序性能
+
+fun main(args: Array<String>) {
+
+}
+
